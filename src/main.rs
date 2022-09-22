f7bc5476710e79fa7bdd16a2ab8fb33a5f7a3ee5
Tsuyoshi.A <perilla.k@gmail.com>
2022-09-22 22:21:55 +0900

    パイプライン入力の出力に対応

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::io::Write;
 use isatty::stdin_isatty;
 
 fn main() {
     if stdin_isatty() {
         println!("terminal");
     } else {
-        println!("pipeline");
+        let reader = std::io::stdin();
+        let mut out = std::io::stdout().lock();
+
+        let mut cnt = 1;
+
+        for line in reader.lines() {
+            writeln!(out, "{0: >5}: {1:}", cnt, line.unwrap_or(String::from("Failed to read line"))).unwrap();
+            cnt += 1;
+        }
     }
 }
