40678d4b6b69bb4f10d2a82640dd040f319f34dc
Tsuyoshi.A <perilla.k@gmail.com>
2022-09-27 21:01:24 +0900

    ファイルの分割と行数に色を設定

---
+++ src/console.rs
@@ -0,0 +1,53 @@
+// const COLORS: [&str; 8] = ["\x1b[30m", "\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[37m"];
+
+pub enum Colors {
+    Black = 0,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+}
+
+pub fn get_color(color: Colors) -> &'static str {
+    match color {
+        Colors::Black => "\x1b[30m",
+        Colors::Red => "\x1b[31m",
+        Colors::Green => "\x1b[32m",
+        Colors::Yellow => "\x1b[33m",
+        Colors::Blue =>  "\x1b[34m",
+        Colors::Magenta => "\x1b[35m",
+        Colors::Cyan => "\x1b[36m",
+        Colors::White => "\x1b[37m",
+    }
+}
+
+pub fn get_back_color(color: Colors) -> &'static str {
+    match color {
+        Colors::Black => "\x1b[40m",
+        Colors::Red => "\x1b[41m",
+        Colors::Green => "\x1b[42m",
+        Colors::Yellow => "\x1b[43m",
+        Colors::Blue =>  "\x1b[44m",
+        Colors::Magenta => "\x1b[45m",
+        Colors::Cyan => "\x1b[46m",
+        Colors::White => "\x1b[47m",
+    }
+}
+
+pub fn ncat_errmsg(msg: String) {
+    println!("{}[ncat error]{}: {}", get_color(Colors::Red), get_color(Colors::White), msg);
+}
+
+// マクロはルート(ncat::)公開となるため use キーワードを使って省略できない
+#[macro_export]
+macro_rules! line_number_output {
+    ($o: ident, $l: expr, $s: expr) => (writeln!($o, "{0:}{1: >5}{2:}: {3:}",
+        ncat::console::get_color(ncat::console::Colors::Yellow),
+        $l,
+        ncat::console::get_color(ncat::console::Colors::White),
+        $s).unwrap())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,8 @@ use std::io::{Write, BufRead};
 use std::fs::File;
 use isatty::stdin_isatty;
 
-const COLORS: [&str; 8] = ["\x1b[30m", "\x1b[31m", "\x1b[32m", "\x1b[33m", "\x1b[34m", "\x1b[35m", "\x1b[36m", "\x1b[37m"];
+use ncat::console;
 
-enum ConsoleColor {
-    Black = 0,
-    Red,
-    Green,
-    Yellow,
-    Blue,
-    Magenta,
-    Cyan,
-    White,
-}
-
-macro_rules! line_number_output {
-    ($o: ident, $l: expr, $s: expr) => (writeln!($o, "{0: >5}: {1:}", $l, $s).unwrap())
-}
-
-fn get_color(color: ConsoleColor) -> &'static str {
-    match color {
-        ConsoleColor::Black => "\x1b[30m",
-        ConsoleColor::Red => "\x1b[31m",
-        ConsoleColor::Green => "\x1b[32m",
-        ConsoleColor::Yellow => "\x1b[33m",
-        ConsoleColor::Blue =>  "\x1b[34m",
-        ConsoleColor::Magenta => "\x1b[35m",
-        ConsoleColor::Cyan => "\x1b[36m",
-        ConsoleColor::White => "\x1b[37m",
-    }
-}
-
-fn get_back_color(color: ConsoleColor) -> &'static str {
-    match color {
-        ConsoleColor::Black => "\x1b[40m",
-        ConsoleColor::Red => "\x1b[41m",
-        ConsoleColor::Green => "\x1b[42m",
-        ConsoleColor::Yellow => "\x1b[43m",
-        ConsoleColor::Blue =>  "\x1b[44m",
-        ConsoleColor::Magenta => "\x1b[45m",
-        ConsoleColor::Cyan => "\x1b[46m",
-        ConsoleColor::White => "\x1b[47m",
-    }
-}
-
-fn ncat_errmsg(msg: String) {
-    println!("{}[ncat error]{}: {}", get_color(ConsoleColor::Red), get_color(ConsoleColor::White), msg);
-}
 
 fn main() {
     let mut cnt = 1;
@@ -67,7 +23,7 @@ fn main() {
         let f = match f {
             Ok(file) => file,
             Err(error) => {
-                ncat_errmsg(error.to_string());
+                console::ncat_errmsg(error.to_string());
                 std::process::exit(1);
             }
         };
@@ -75,7 +31,7 @@ fn main() {
         let reader = std::io::BufReader::new(f);
 
         for line in reader.lines() {
-            line_number_output!(out, cnt, line.unwrap_or(String::from("Failed to read line.")));
+            ncat::line_number_output!(out, cnt, line.unwrap_or(String::from("Failed to read line.")));
             cnt += 1;
         }
     } else {
@@ -83,7 +39,7 @@ fn main() {
 
         for line in reader.lines() {
             // writeln!(out, "{0: >5}: {1:}", cnt, line.unwrap_or(String::from("Failed to read line."))).unwrap();
-            line_number_output!(out, cnt, line.unwrap_or(String::from("Failed to read line.")));
+            ncat::line_number_output!(out, cnt, line.unwrap_or(String::from("Failed to read line.")));
             cnt += 1;
         }
     }
